c6386230c7528c34cd50a9cf5cd2dac60ef91cf8
Michael M. Hansen <59628075+mmhansen137@users.noreply.github.com>
2020-02-26 20:09:08 -0800

    Adds user interface.

--- tempThing.py
+++ tempThing.py
@@ -6,3 +6,18 @@ def f2c(fTemp):
 
 def c2f(cTemp):
     return ((cTemp * 9 / 5) + 32)
+
+def main():
+    print("Temperature Converter")
+    convChoice = input("Enter C for Celsius to Fahrenheit, F for Fahrenheit to Celsius, or E to exit: ")
+    if convChoice == "C":
+        print(c2f(float(input("Enter temperature in Celsius: "))), "F")
+        main()
+    elif convChoice == "F":
+        print(f2c(float(input("Enter temperature in Fahrenheit: "))), "C")
+        main()
+    elif convChoice == "E":
+        exit()
+    else: main()
+
+main()
