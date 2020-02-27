8119985b3e6018a622364513d5e0f3ef3f19c9f3
Michael M. Hansen <59628075+mmhansen137@users.noreply.github.com>
2020-02-26 19:46:30 -0800

    Removes unnecessary parentheses.

--- tempThing.py
+++ tempThing.py
@@ -1,8 +1,8 @@
 # tempThing.py
 
 def f2c(fTemp):
-    return (((fTemp - 32) * 5) / 9)
+    return ((fTemp - 32) * 5 / 9)
     
 
 def c2f(cTemp):
-    return (((cTemp *9) / 5) + 32)
+    return ((cTemp * 9 / 5) + 32)
